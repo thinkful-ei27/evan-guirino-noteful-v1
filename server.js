06a6fec47c4672f27007545beeb5131658e774a9
Evan Guirino <etg624@gmail.com>
2018-12-17 11:48:27 -0800

    started server

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 'use strict';
-
+const express = require('express');
 // Load array of notes
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+const app = express();
+
+app.use(express.static('public'))
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`)  
+})
+.on('error',  function (err) {
+  console.log(err);
+});
+
 
 // INSERT EXPRESS APP CODE HERE...
